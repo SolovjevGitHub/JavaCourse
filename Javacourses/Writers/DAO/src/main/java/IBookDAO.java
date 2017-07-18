d8a53988a75793a87c142a500bb2d5bafc69d59f
SolovjevGitHub <chester2010g@mail.ru>
2017-07-19 01:51:11 +0300

    version 3
    Signed-off-by: SolovjevGitHub <chester2010g@mail.ru>

--- Javacourses/Writers/DAO/src/main/java/IBookDAO.java
+++
@@ -1,9 +0,0 @@
-
-import java.sql.Connection;
-import java.util.List;
-
-public interface IBookDAO extends IGenericDAO<Book,Integer> {
-    List<Book> getAllBooksByWriterName(String name, Connection connection);
-    List<Book> getBookByWriterId(int wtiterId, Connection connection);
-    Book getBookInfoByTitle(String title, Connection connection);
-}
